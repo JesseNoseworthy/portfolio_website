5d966429928d30d287af10dc5ab9ae22e59280f3
Jesse Noseworthy <noseworthyjesse@gmail.com>
2016-06-07 17:49:30 -0400

    about section roughly in place

--- front-page.php
+++ front-page.php
@@ -1,8 +1,8 @@
 <?php get_header();  ?>
 	<!-- Import hero header -->
 	<?php include 'hero.php'; ?>
-	
-
+	<!-- Import about section -->
+	<?php include 'about_section.php'; ?>
 	<!-- Import contact section -->
 	<?php include 'contact_section.php' ?>
 <?php get_footer(); ?>
