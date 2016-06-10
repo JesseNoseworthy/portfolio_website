83b52404286bcd35884fe6dfb46361fea97ea848
Jesse Noseworthy <noseworthyjesse@gmail.com>
2016-06-10 16:52:08 -0400

    updated 404

--- 404.php
+++ 404.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 
 <div class="main">
-  <div class="container">
-
-    <h1>Not Found</h1>
-    <p>Apologies, but the page you requested could not be found. Perhaps searching will help.</p>
-    <?php get_search_form(); ?>
-
+	<?php $background = wp_get_attachment_image_src( get_post_thumbnail_id( 5 ), 'full' ); ?>
+  <div id="not-found-hero" style="background: linear-gradient(rgba(57,57,57,0.5), rgba(57,57,57,0.5)), url('<?php echo $background[0]; ?>'); ?> ; background-size: cover;">
+	  <div class="not-found">
+		  <h1 class="not-found-head">404</h1>
+		  <h2 class="not-found-sub">Page not found</h2>
+		  <h4 class="not-found-text">You can go back <a href="jessenoseworthy.com">home</a> or try to search below:</h4>
+		  <?php get_search_form(); ?>
+		</div>
   </div> <!-- /.container -->
 </div> <!-- /.main -->
-
-<?php get_footer(); ?>
